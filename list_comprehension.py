7b8b5dd5859168936f42c1195668316d1f5d501f
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-06-19 06:43:40 -0400

    Update list_comprehension.py
    almost done

--- list_comprehension.py
+++ list_comprehension.py
@@ -22,12 +22,18 @@ print("FILTERED LIST W/ MATCHES:", filtered_list)
 
 print("---------------")
 
-
 unfiltered_list = [numbers for numbers in my_numbers if numbers not in my_numbers]
 
 print("FILTERED LIST W/O MATCHES:", unfiltered_list)
 
 print("---------------")
+
+#mappednfiltered = [nums for nums in numbersby100 if nums > 300]
+#print(mappednfiltered)
+
+
+print("MAPPED AND FILTERED LIST:", mapnfilt)
+
 
 #Use mapping capabilities to multiply each number by 100 (e.g. [100, 200, 300, 400, 500, 600, 700])
 #Use filtering capabilities to return only the numbers greater than three (e.g. [4, 5, 6, 7])
